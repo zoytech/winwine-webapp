3529ed4c8bde765df8be844646622699d702c3b6
namle <namlh4@vng.com.vn>
2022-10-30 19:30:35 +0700

    feat:init project by reccommended spring boot

---
+++ src/main/java/com/zoytech/winwine/webapp/WinwineApplication.java
@@ -0,0 +1,13 @@
+package com.zoytech.winwine.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinwineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WinwineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zoytech/winwine/webapp/WinwineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoytech.winwine.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinwineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
